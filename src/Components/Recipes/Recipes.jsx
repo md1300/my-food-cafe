fc2ad9271ccf92878143222b1eb522f74acf1dea
md1300 <md.tawhid.khan1998@gmail.com>
2024-06-06 00:00:59 +0600

    third commit

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -3,13 +3,58 @@ import { useEffect, useState } from "react";
 
 const Recipes = () => {
     const [recipes,setRecipes]=useState([])
+
+    const [wantCooks,setWantCooks]=useState([])
+
+    const [startCooking,setStartCooking]=useState([])
+
+    const [times,setTimes]=useState(0)
+
+    const [calories,setCalories]=useState(0)
+    
      
     useEffect(()=>{
         fetch('Recipes.json')
         .then(res=>res.json())
         .then(data=>setRecipes(data))
     },[])
+
+   const handleWantToCook=wantToCook=>{
+
+    const isExist= wantCooks.find(findCook=>findCook.id==wantToCook.id)
+    if(!isExist){
+        setWantCooks([...wantCooks,wantToCook])
+    }
+    else{
+        alert('already existed')
+    }
+       
+   }
+
+const  handleRemoveCooking=wantCook=>{
+   const remainingCooking=wantCooks.filter(remainingCook=>remainingCook.id!==wantCook.id  ) 
+   setWantCooks(remainingCooking)
+
+}
+
+const handleStartCooking=(cooking)=>{
+ const newStartCooking=[...startCooking,cooking]
+ setStartCooking(newStartCooking)
  
+}
+
+
+const handleTotalTime=(time)=>{
+      setTimes(times+parseInt(time.time))
+      console.log(times)
+}
+
+const handleTotalCalories=(calorie)=>{
+    setCalories(calories+parseInt(calorie.calories))
+}
+
+
+
     return (
         <div className="m-5">
             {/* our header started */}
@@ -23,18 +68,41 @@ const Recipes = () => {
 
                 {
                     recipes.map(recipe=><div key={recipe.id}>
-                        
+                       <img src={recipe.image} alt="" srcset="" /> 
                         <h3>{recipe.title}</h3>
                         <h5>time : {recipe.time}</h5>
                         <h5>calories : {recipe.calories}</h5>
-                        <button className="bg-orange-100">Want to cook</button>
+                        <button onClick={()=>handleWantToCook(recipe)} className="bg-orange-100">Want to cook</button>
                     </div>)
                 }
                 
             </div>
 
             <div>
-                <h1>Want To Cook</h1>
+                <h1>Want To Cook {wantCooks.length}</h1>
+                {
+                    wantCooks.map(wantCook=><div key={wantCook.id}>
+                        <h1>{wantCook.title}</h1>
+                        <h3>{wantCook.title}minutes</h3>
+                        <h3>{wantCook.calories} calories</h3>
+                        <button onClick={()=>{handleRemoveCooking(wantCook),handleStartCooking(wantCook),handleTotalTime(wantCook),handleTotalCalories(wantCook)}} className="bg-stone-800 text-white">preparing</button>
+                    </div>)
+                }
+            </div>
+            <div>
+                <h1>Currently Cooking : {startCooking.length}</h1>
+                {
+                  startCooking.map(cooking=><div key={cooking.id}>
+                    <h2>{cooking.title}</h2>
+                  </div>)  
+                }
+            </div>
+
+            <div>
+                 <h1>total time : {times}</h1>
+            </div>
+            <div>
+                <h1>total calories : {calories}</h1>
             </div>
             
         </div>
