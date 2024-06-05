4fdccdc9076d0c080f0363690030d1eb52ceca66
md1300 <md.tawhid.khan1998@gmail.com>
2024-06-06 00:59:05 +0600

    fourth commit

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-
+import { CgAlarm } from "react-icons/cg";
+import { AiOutlineFire } from "react-icons/ai";
 
 const Recipes = () => {
     const [recipes,setRecipes]=useState([])
@@ -56,64 +57,81 @@ const handleTotalCalories=(calorie)=>{
 
 
     return (
-        <div className="m-5">
+     <div className="m-5">
             {/* our header started */}
             <div className="text-center mb-8">
             <h1 className="text-3xl "> Our Recipes </h1>
             <p>With the help of soy sauce, Dijon mustard, and vinegar, a jar of apricot jam transforms chicken legs into a gorgeous weeknight dinner.</p>
             </div>
         {/* our header ended */}
-            <div>
-                <h1>Total Recipes :{recipes.length} </h1>
+        <h1 className="text-3xl text-center mb-7">Total Recipes :{recipes.length} </h1>
+        <div className="flex ">
+         <div className=" w-2/3" >
+              
+            <div  className="grid grid-cols-2 gap-4 mt-7 p-6 ">             
+                  {
+                      recipes.map(recipe=><div className=" bg-slate-100 " key={recipe.id}>
+                          <img  src={recipe.img} alt="" srcset="" /> 
+                           <h3 className="m-4">{recipe.title}</h3>
+                           <p className="mb-4">{recipe.description}</p>
+                        <div className="m-4">
+                               <h3>ingredient : {recipe.ingredients.length}</h3>
+                               {
+                                 recipe.ingredients.map((ingredient, index)=><div key={index}>
+                                <ul>{ingredient}</ul>
+                                </div>)
+                               } 
+                         </div>
 
-                {
-                    recipes.map(recipe=><div key={recipe.id}>
-                       <img src={recipe.img} alt="" srcset="" /> 
-                        <h3>{recipe.title}</h3>
-                        <p>{recipe.description}</p>
-                        <h3>ingredient : {recipe.ingredients.length}</h3>
-                       {
-                        recipe.ingredients.map((ingredient, index)=><div key={index}>
-                            <ul>{ingredient}</ul>
-                        </div>)
-                       }
-
-                        <h5>time : {recipe.time}</h5>
-                        <h5>calories : {recipe.calories}</h5>
-                        <button onClick={()=>handleWantToCook(recipe)} className="bg-orange-100">Want to cook</button>
+                          
+                       <div className="flex items-center gap-5 my-4">
+                            <div className="flex items-center gap-4">
+                              <CgAlarm />
+                              <h5> time : {recipe.time} minutes</h5>
+                            </div>
+                            <div className="flex items-center gap-4">
+                             <AiOutlineFire />
+                             <h5>calories : {recipe.calories}</h5>
+                            </div>
+                        
+                       </div>
+                        
+                        <button onClick={()=>handleWantToCook(recipe)} className="bg-green-400 rounded-3xl mb-7">Want to cook</button>
                     </div>)
                 }
                 
             </div>
-
-            <div>
+         </div>
+         <div className="1/3">
+              <div>
                 <h1>Want To Cook {wantCooks.length}</h1>
-                {
-                    wantCooks.map(wantCook=><div key={wantCook.id}>
-                        <h1>{wantCook.title}</h1>
-                        <h3>{wantCook.title}minutes</h3>
-                        <h3>{wantCook.calories} calories</h3>
-                        <button onClick={()=>{handleRemoveCooking(wantCook),handleStartCooking(wantCook),handleTotalTime(wantCook),handleTotalCalories(wantCook)}} className="bg-stone-800 text-white">preparing</button>
-                    </div>)
-                }
-            </div>
-            <div>
+                    {
+                        wantCooks.map(wantCook=><div key={wantCook.id}>
+                            <h1>{wantCook.title}</h1>
+                            <h3>{wantCook.title}minutes</h3>
+                            <h3>{wantCook.calories} calories</h3>
+                            <button onClick={()=>{handleRemoveCooking(wantCook),handleStartCooking(wantCook),handleTotalTime(wantCook),handleTotalCalories(wantCook)}} className="bg-stone-800 text-white">preparing</button>
+                          </div>)
+                    }
+              </div>
+              <div>
                 <h1>Currently Cooking : {startCooking.length}</h1>
-                {
-                  startCooking.map(cooking=><div key={cooking.id}>
-                    <h2>{cooking.title}</h2>
-                  </div>)  
-                }
-            </div>
+                  {
+                      startCooking.map(cooking=><div key={cooking.id}>
+                       <h2>{cooking.title}</h2>
+                      </div>)  
+                  }
+              </div>
 
-            <div>
+              <div>
                  <h1>total time : {times}</h1>
-            </div>
-            <div>
+              </div>
+              <div>
                 <h1>total calories : {calories}</h1>
-            </div>
-            
-        </div>
+              </div>
+          </div> 
+        </div>    
+    </div>
     );
 };
 
